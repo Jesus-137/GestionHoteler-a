ab401b345f5b3413cfc71b9d80b8df8d97384aa5
Jesus Velazquez <velazquezhernandezjesus13@gmail.com>
2025-09-13 13:08:39 -0600

    Creación de proyecto

---
+++ backend_hotelera/src/test/java/com/hotelera/backend/BackendHoteleraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelera.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHoteleraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
