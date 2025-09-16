707f4e46463f7e0871e0e251bb07c362a21565ec
Mariana Colio <adrimar0127@outlook.com>
2025-09-15 22:15:08 -0600

    Añadí dependencias al poom, cree paquetes, y configure el archivo de properties

---
+++ backend_hotelera/msv-huespedes/src/test/java/com/example/huspedes/MsvHuespedesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.huspedes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvHuespedesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
