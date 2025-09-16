21a6f780921b2baded0d4b1865a670f8795538f5
Mariana Colio <adrimar0127@outlook.com>
2025-09-15 23:11:24 -0600

    Creación del controller, creación del localhandler y configuración de seguridad

---
+++ backend_hotelera/msv-huespedes/src/main/java/com/example/huespedes/controllers/LocalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.huespedes.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.Map;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@RestController
+public class LocalExceptionHandler {
+
+	private static final Logger LOGGER = Logger.getLogger(LocalExceptionHandler.class.getName());
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+public ResponseEntity<Map<String, Object>> handleDataIntegrityViolationException(DataIntegrityViolationException e){
+		LOGGER.log(Level.SEVERE, "Error en la integridad de los datos: " +
+(e.getCause() != null ? e.getCause() : e.getMessage()));
+				return ResponseEntity.badRequest().body(Map.of(
+						"code", HttpStatus.BAD_REQUEST.value(),
+						"response","Error en la integridad de los datos: " + e.getMessage()
+						));
+	}
+	
+}
