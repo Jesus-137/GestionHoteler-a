dd270fa6c21327d45dd9f7885b66598f47ad3313
Jesus Velazquez <velazquezhernandezjesus13@gmail.com>
2025-09-15 18:54:15 -0600

    estructura del autorization server

---
+++ backend_hotelera/authorization-server/src/main/java/com/jesus/authorization/dto/LoginRequest.java
@@ -0,0 +1,7 @@
+package com.christian.oauth.dto;
+
+public record LoginRequest(
+		String username,
+		String password
+)
+{}
---
+++ backend_hotelera/authorization-server/src/test/java/com/jesus/authorization/AuthorizationServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
