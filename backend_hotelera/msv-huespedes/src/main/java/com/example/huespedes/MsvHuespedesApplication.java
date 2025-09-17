54151a7ab26dfcc13464563642c6db102a64170f
Mariana Colio <adrimar0127@outlook.com>
2025-09-16 21:17:34 -0600

    Correccion de msv-microservicio terminado, agregando el main 2

--- backend_hotelera/msv-huespedes/src/main/java/com/example/huespedes/MsvHuespedesApplication.java
+++ backend_hotelera/msv-huespedes/src/main/java/com/example/huespedes/MsvHuespedesApplication.java
@@ -1,4 +1,4 @@
-package com.example.huspedes;
+package com.example.huespedes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
