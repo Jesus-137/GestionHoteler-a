89dce2d82bd0489f727a30ce30dc92f0e1dbacda
Jesus Velazquez <velazquezhernandezjesus13@gmail.com>
2025-09-17 23:43:25 -0600

    creacion del micro servicio reservas

---
+++ backend_hotelera/msv-reserva/src/main/java/com/hotelera/reserva/MsvReservaApplication.java
@@ -0,0 +1,13 @@
+package com.hotelera.reserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvReservaApplication.class, args);
+	}
+
+}
